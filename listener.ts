2a165c9f273d2c5c264c0360321357d05eaa366a
stackinspector <stackinspector@126.com>
2020-12-26 05:07:07 +0800

    better type and fit latest fetchlot (v7)

--- listener.ts
+++ listener.ts
@@ -1,5 +1,5 @@
 import { writelnSync } from 'baseutil/writeln.ts'
-import type { Ports, Output } from 'baseutil/fetchlot.ts'
+import type { Dict, Ports, Output } from 'baseutil/fetchlot.ts'
 import { worker } from 'baseutil/fetchlot.ts'
 
 const ID = 16378017
@@ -9,27 +9,33 @@ const ports: Ports<number> = new Map([
         url: () => `https://api.bilibili.com/x/tag/info?tag_id=${ID}`,
         valid: [
             (resp) => resp.code as number === 0,
-            (resp) => resp.data.tag_id as number === ID,
+            (resp) => (resp.data as Dict).tag_id as number === ID,
         ],
-        proc: (resp) => ({
-            use: resp.data.count.use as number,
-            follow: resp.data.count.atten as number,
-        }),
+        proc: (resp) => {
+            const count = (resp.data as Dict).count as Dict
+            return {
+                use: count.use as number,
+                follow: count.atten as number,
+            }
+        },
     }],
     ['extd', {
         url: () => `https://api.vc.bilibili.com/topic_svr/v1/topic_svr/get_active_users?topic_id=${ID}`,
         valid: [
             (resp) => resp.code as number === 0,
-            (resp) => resp.data.topic_id as number === ID,
+            (resp) => (resp.data as Dict).topic_id as number === ID,
         ],
-        proc: (resp, input) => ({
-            view: resp.data.view_count as number,
-            discuss: resp.data.discuss_count as number,
-            active: new Date(input).getMinutes() % 10 === 0 ? (resp.data.active_users as any[]).map((s) => ({
-                uid: s.user_info.uid as number,
-                score: s.score as number,
-            })) : void 0,
-        }),
+        proc: (resp, input) => {
+            const data = resp.data as Dict
+            return {
+                view: data.view_count as number,
+                discuss: data.discuss_count as number,
+                active: new Date(input).getMinutes() % 10 === 0 ? (data.active_users as Dict[]).map((s) => ({
+                    uid: (s.user_info as Dict).uid as number,
+                    score: s.score as number,
+                })) : void 0,
+            }
+        },
     }],
 ])
 
